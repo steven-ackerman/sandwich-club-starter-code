5077eb45ffc5dc8ce5a1e77743a0462d6199ea20
Steven Ackerman <ackerman.steven@gmail.com>
2018-04-28 15:06:21 -0700

    First Commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.TextView;
+
+import java.util.List;
+
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
@@ -14,6 +18,12 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    /**Class Variables **/
+    TextView mAlsoKnownAs;
+    TextView mPlaceOfOrigin;
+    TextView mDescription;
+    TextView mIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +53,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +66,61 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        //Sets the Description Texview for the sandwich object. Checks for empty & null.
+
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        String description = sandwich.getDescription();
+        if (description == null || description.equals("")) {
+            description = getString(R.string.no_description);
+        }else
+        descriptionTextView.setText(description);
+
+        //Sets the TexView, Checks for empty List & Creates
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        String ingredients;
+        List<String> ingredientsList = sandwich.getIngredients();
+        if (ingredientsList.isEmpty()) {
+            ingredients = getString(R.string.unknown_ingredients);
+        } else {
+            //Builds a string from the characters:
+            StringBuilder ingredientString = new StringBuilder();
+            for (int i = 0; i < ingredientsList.size(); i++) {
+                ingredientString
+                        .append(ingredientsList.get(i));
+                if (i != ingredientsList.size() - 1) {
+                    ingredientString.append('\n');
+                }//End if.
+            }//End for loop.
+            ingredients = ingredientString.toString();
+        }
+        ingredientsTextView.setText(ingredients);
+
+        TextView placeOfOriginTextView = findViewById(R.id.origin_tv);
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        //Null OR Empty Check.
+        if (placeOfOrigin == null || placeOfOrigin.equals("")) {
+            placeOfOrigin = getString(R.string.unknown_origin);
+        }
+        placeOfOriginTextView.setText(placeOfOrigin);
+
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        String alsoKnownAs;
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        if (alsoKnownAsList.isEmpty()) {
+            alsoKnownAs = getString(R.string.no_aliases);
+        } else {
+            StringBuilder akaBuilder = new StringBuilder();
+            for (int i = 0; i < alsoKnownAsList.size(); i++) {
+                akaBuilder
+                        .append(alsoKnownAsList.get(i));
+                if (i != alsoKnownAsList.size() - 1)
+                    akaBuilder.append('\n');
+            }
+            alsoKnownAs = akaBuilder.toString();
+        }
+        alsoKnownAsTextView.setText(alsoKnownAs);
     }
+
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,64 @@
 package com.udacity.sandwichclub.utils;
 
+//Imports:
+
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws RuntimeException {
+        Sandwich sandwich = null;
+        try {
+            //Parses each appropriate string from the json string.
+            JSONObject sandwichJSON = new JSONObject(json);
+            //Gets the all the object details and mainName from the JSON string.
+            JSONObject name = sandwichJSON.getJSONObject("name");
+            String mainName = sandwichJSON.getString("mainName");
+
+            //Creates a JSON Array of AKA & Ingredients data. Calls Method to convert to List<String>.
+            JSONArray alsoKnownAsJSON = sandwichJSON.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = convertJsonArray(alsoKnownAsJSON);
+            Log.d("List Contents:", alsoKnownAs.toString());
+
+            JSONArray ingredientsJson = sandwichJSON.getJSONArray("ingredients");
+            List<String> ingredients = convertJsonArray(ingredientsJson);
+            Log.d("Ingredients List", ingredients.toString());
+
+
+            //Gets the remaining details from the JSON Object needed for the sandwich Constructor.
+
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
+            String description = sandwichJSON.getString("description");
+            String image = sandwichJSON.getString("image");
+
+            //Creates sandwich Object with parsed data.
+            sandwich = new Sandwich(mainName, alsoKnownAs,placeOfOrigin,description,image,ingredients);
+            } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
+    /* Converts JSON Array to a List of Type String. */
+    private static List<String> convertJsonArray(JSONArray jsonArray) {
+        int length = jsonArray.length();
+        List<String> converted = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            try {
+                converted.add(jsonArray.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return converted;
+    }//End Convert Json Array Method.
 }
