9025547f270f3ea3287197bf21c959e1fc36855a
Steven Ackerman <ackerman.steven@gmail.com>
2018-04-29 19:04:47 -0700

    Label & Data Alignment needs work

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -20,19 +20,20 @@ public class JsonUtils {
         Sandwich sandwich = null;
         try {
             //Parses each appropriate string from the json string.
-            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject sandwichJSON = new JSONObject(json);//1.
             //Gets the all the object details and mainName from the JSON string.
-            JSONObject name = sandwichJSON.getJSONObject("name");
-            String mainName = sandwichJSON.getString("mainName");
+            JSONObject name = sandwichJSON.getJSONObject("name");//2
+            String mainName = name.getString("mainName");
 
             //Creates a JSON Array of AKA & Ingredients data. Calls Method to convert to List<String>.
-            JSONArray alsoKnownAsJSON = sandwichJSON.getJSONArray("alsoKnownAs");
+            JSONArray alsoKnownAsJSON = name.getJSONArray("alsoKnownAs");
             List<String> alsoKnownAs = convertJsonArray(alsoKnownAsJSON);
             Log.d("List Contents:", alsoKnownAs.toString());
 
             JSONArray ingredientsJson = sandwichJSON.getJSONArray("ingredients");
             List<String> ingredients = convertJsonArray(ingredientsJson);
             Log.d("Ingredients List", ingredients.toString());
+
 
 
             //Gets the remaining details from the JSON Object needed for the sandwich Constructor.
